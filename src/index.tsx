5f0a9031b0b762365e06d6e156bf96528f4f60f9
rosslincoln-create <ross.lincoln60@gmail.com>
2026-04-07 21:38:53 +1000

    Task 0 edit 1: Add STRIPE_PRICE_ID to Env type
    Add STRIPE_PRICE_ID to environment variables

--- src/index.tsx
+++ src/index.tsx
@@ -12,6 +12,7 @@ export type Env = {
   R2: R2Bucket
   BROWSER: Fetcher
   OPENAI_API_KEY: string
+  STRIPE_PRICE_ID: string
   STRIPE_SECRET_KEY: string
   STRIPE_WEBHOOK_SECRET: string
   RESEND_API_KEY: string
